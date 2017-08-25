cb0e6ccf453dddb75a82c38aae1112173de31b54
gregmeszaros <meszaros275@gmail.com>
2017-08-25 16:57:21 +0100

    Return the correct Redis instance

--- RedisInit.php
+++ RedisInit.php
@@ -2,5 +2,6 @@
   // @TODO all the hosts, ports etc can come from config file?
   // Init REDIS
   $redis = new Redis();
-  return $redis->connect('127.0.0.1', 6379);
+  $redis->connect('127.0.0.1', 6379);
+  return $redis;
 ?>

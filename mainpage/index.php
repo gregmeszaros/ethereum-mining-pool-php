72c5c5ffb67fd316d04e8658726fdf949d654daf
gregmeszaros <meszaros275@gmail.com>
2017-08-30 10:54:10 +0100

    missing function

--- mainpage/index.php
+++ mainpage/index.php
@@ -190,4 +190,14 @@ switch ($method) {
     break;
 }
 
+function bcdechex($dec) {
+  $hex = '';
+  do {
+    $last = bcmod($dec, 16);
+    $hex = dechex($last) . $hex;
+    $dec = bcdiv(bcsub($dec, $last), 16);
+  } while($dec > 0);
+  return $hex;
+}
+
 ?>

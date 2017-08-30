8ee412939fd149f973d62615930e89d59452b158
gregmeszaros <meszaros275@gmail.com>
2017-08-30 09:45:53 +0100

    getWork

--- mainpage/index.php
+++ mainpage/index.php
@@ -54,6 +54,7 @@ $method = $json['method'] ?? '';
 
 if($log) {
   $current .= "\n Method used: $method";
+  $current .= "\n PHP input: " . print_r($json, TRUE);
   file_put_contents($log_path, $current);
 }
 
@@ -96,9 +97,35 @@ switch ($method) {
       'Content-Length: ' . strlen($data))
     );
 
+    // Block info
     $output = curl_exec($ch_block);
+
     if($log) {
       $current .= "\n eth_getBlockByNumber: " . print_r($output, TRUE);
+      file_put_contents($log_path, $current);
+    }
+
+    $data = [
+      "jsonrpc" => "2.0",
+      "method" => "eth_getWork",
+      "params" => [], "id" => 73
+    ];
+    $data = json_encode($data);
+
+    $ch_get_work = curl_init('http://127.0.0.1:8983');
+    curl_setopt($ch_get_work, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch_get_work, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch_get_work, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch_get_work, CURLOPT_HTTPHEADER, array(
+      'Content-Type: application/json',
+      'Content-Length: ' . strlen($data))
+    );
+
+    // eth_getWork
+    $output = curl_exec($ch_get_work);
+
+    if($log) {
+      $current .= "\n eth_getWork: " . print_r($output, TRUE);
       file_put_contents($log_path, $current);
     }
 

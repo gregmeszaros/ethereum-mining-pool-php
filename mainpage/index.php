eb32e2c99ca53baba897b3f431cc49263d428d63
gregmeszaros <meszaros275@gmail.com>
2017-08-30 16:52:48 +0100

    difficulty calc

--- mainpage/index.php
+++ mainpage/index.php
@@ -148,13 +148,27 @@ switch ($method) {
 
     $output = json_decode($output, TRUE);
     $redis->set('getWorkPow', $output['result'][0]);
-    $output['result'][2] = getTargetDiff();
+    $new_diff = getTargetDiff();
+    $fix = '';
+
+    $target_diff = bcdechex($new_diff);
+    $currentLenght = strlen($target_diff);
+    $desiredLenght = 64;
+    if ($currentLenght < $desiredLenght) {
+      $toadd = $desiredLenght - $currentLenght;
+      for ($i=0; $i < $toadd; $i++) {
+        $fix .= '0';
+      }
+      $target_diff = '0x' . $fix . $target_diff;
+    }
+
+    $output['result'][2] = $target_diff;
 
     echo json_encode($output);
 
     if($log) {
       $current .= "\n eth_getWork: " . print_r($output, TRUE);
-      $current .= "\n eth_getWork : target diff " . print_r(getTargetDiff(), TRUE);
+      $current .= "\n eth_getWork : target diff " . print_r($target_diff), TRUE);
       file_put_contents($log_path, $current);
     }
 
@@ -209,14 +223,24 @@ switch ($method) {
  * @param int $miner_hashrate
  * @return String
  */
-function getTargetDiff($pool_diff = 2000000000, $miner_hashrate = 1) {
+function getTargetDiff($pool_diff = 20000000, $miner_hashrate = 1) {
   $a256 = new Math_BigInteger('115792089237316195423570985008687907853269984665640564039457584007913129639936');  //2^256
   $pool_diff = new Math_BigInteger($pool_diff * $miner_hashrate);
 
   list($quotient, $remainder) = $a256->divide($pool_diff);
   $target_diff = new Math_BigInteger($quotient->toString());
 
-  return $target_diff->toHex();
+  return $target_diff;
+}
+
+function bcdechex($dec) {
+  $hex = '';
+  do {
+    $last = bcmod($dec, 16);
+    $hex = dechex($last) . $hex;
+    $dec = bcdiv(bcsub($dec, $last), 16);
+  } while($dec > 0);
+  return $hex;
 }
 
 ?>

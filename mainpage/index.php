ada8a4d74514abdec613a6d2bfb8557c00bb601d
gregmeszaros <meszaros275@gmail.com>
2017-08-30 21:02:00 +0100

    Diff 4billion

--- mainpage/index.php
+++ mainpage/index.php
@@ -223,7 +223,7 @@ switch ($method) {
  * @param int $miner_hashrate
  * @return String
  */
-function getTargetDiff($pool_diff = 200000000, $miner_hashrate = 1) {
+function getTargetDiff($pool_diff = 4000000000, $miner_hashrate = 1) {
   $a256 = new Math_BigInteger('115792089237316195423570985008687907853269984665640564039457584007913129639936');  //2^256
   $pool_diff = new Math_BigInteger($pool_diff * $miner_hashrate);
 

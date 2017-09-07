cadc3a987c9f9b6915f4f14399fb179116643a40
gregmeszaros <meszaros275@gmail.com>
2017-09-07 11:16:39 +0100

    process

--- block_processing/process_block/index.php
+++ block_processing/process_block/index.php
@@ -3,24 +3,52 @@
 // Include REDIS
 $redis = include_once('../../RedisInit.php');
 
-$data = [
-  "jsonrpc" => "2.0",
-  "method" => "eth_getBlockByNumber",
-  "params" => ["latest", true],
-  "id" => "1"
-];
+$nonces_to_check = json_decode($redis->get('nonces_to_check'));
+$iteration = 0;
 
-$data_string = json_encode($data);
-$block_info = curl_init('http://127.0.0.1:8983');
-curl_setopt($block_info, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($block_info, CURLOPT_POSTFIELDS, $data_string);
-curl_setopt($block_info, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($block_info, CURLOPT_HTTPHEADER, [
-    'Content-Type: application/json',
-    'Content-Length: ' . strlen($data_string)
-  ]
-);
+while(1) {
+  $iteration++;
 
-$result = curl_exec($block_info);
+  $result = getBlock();
+  $last_block = json_decode($result);
+  $last_block_number = hexdec($last_block['number']);
+  print $last_block_number;
+  print 'iteration number: ' . $i;
 
-print_r($result);
+  if (is_array(($nonces_to_check)) {
+    foreach ($nonces_to_check as $nonce) {
+      print_r($nonces_to_check);
+    }
+  }
+
+  // Wait 18 seconds
+  usleep(18000000);
+}
+
+/**
+ * Get data about the block we need
+ * @param string $block_number "latest" or block number in HEX
+ */
+function getBlock($block_number = "latest") {
+
+  $data = [
+    "jsonrpc" => "2.0",
+    "method" => "eth_getBlockByNumber",
+    "params" => [$block_number, true],
+    "id" => "1"
+  ];
+
+  // Get the latest block info first
+  $data_string = json_encode($data);
+  $block_info = curl_init('http://127.0.0.1:8983');
+  curl_setopt($block_info, CURLOPT_CUSTOMREQUEST, "POST");
+  curl_setopt($block_info, CURLOPT_POSTFIELDS, $data_string);
+  curl_setopt($block_info, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($block_info, CURLOPT_HTTPHEADER, [
+      'Content-Type: application/json',
+      'Content-Length: ' . strlen($data_string)
+    ]
+  );
+
+  return curl_exec($block_info);
+}
